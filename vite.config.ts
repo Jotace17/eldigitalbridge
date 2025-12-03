c86e32ef49e3a722d5560245bee492901008469f
Caselles17 <johnathan.caselles@gmail.com>
2025-12-03 18:28:02 +0100

    name update

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/el-puente-digital/', 
+  base: '/eldigitalbridge/', 
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
